a987733c7978d89770c3b4efb97714a207b517b2
Michael Newman <daudadagr@gmail.com>
2016-04-08 12:45:38 -0500

    add basic styling and add mirage package (but mirage isn't being used yet)

--- app/routes/pokemon.js
+++ app/routes/pokemon.js
@@ -1,16 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    // hardcode Bulbasaur
-    return {
-      "id": 1,
-      "name": "bulbasaur",
-      "base_experience": 64,
-      "height": 7,
-      "is_default": true,
-      "order": 1,
-      "weight": 69,
-    };
-  }
+	model() {
+  		// hardcode bulbasaur
+		return {
+			"id": 1,
+			"name": "bulbasaur",
+		};
+	}
 });
