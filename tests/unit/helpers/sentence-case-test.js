1db18dffefad397a4be9697c7b924e99bff69957
Michael Newman <daudadagr@gmail.com>
2016-04-10 15:01:51 -0500

    add more sentence case tests

--- tests/unit/helpers/sentence-case-test.js
+++ tests/unit/helpers/sentence-case-test.js
@@ -7,3 +7,13 @@ test('it returns the empty string if input is null', function(assert) {
   let result = sentenceCase([null]);
   assert.strictEqual(result, '', 'Result of null should be empty');
 });
+
+test('it capitalizes the first letter of the string', function(assert) {
+	let result = sentenceCase(['inputstring']);
+	assert.strictEqual(result, 'Inputstring');
+});
+
+test('it lower-cases subsequent letters of the string', function(assert) {
+	let result = sentenceCase(['iNPUTSTRING']);
+	assert.strictEqual(result, 'Inputstring');
+});
