8111826f4a2c1a7d7cf1aa4b6169faecf356e543
Michael Newman <daudadagr@gmail.com>
2016-04-10 18:26:25 -0500

    add height tests; fix issue #5

--- tests/unit/helpers/height-test.js
+++ tests/unit/helpers/height-test.js
@@ -3,8 +3,17 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | height');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = height([42]);
-  assert.ok(result);
+test('converts input to meters and adds units', function(assert) {
+	let result = height([42]);
+	assert.strictEqual(result, '4.2 m');
+});
+
+test('rounds the height to one decimal place', function(assert) {
+	let result = height([4.231]);
+	assert.strictEqual(result, '0.4 m');
+});
+
+test('always displays one decimal place, even if digit is 0', function(assert) {
+	let result = height([40]);
+	assert.strictEqual(result, '4.0 m');
 });
