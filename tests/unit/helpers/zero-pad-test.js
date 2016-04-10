2b2b8f629ec4039da64b04ac8ac24e17a7e87de1
Michael Newman <daudadagr@gmail.com>
2016-04-10 15:19:39 -0500

    add tests for zero-pad

--- tests/unit/helpers/zero-pad-test.js
+++ tests/unit/helpers/zero-pad-test.js
@@ -3,8 +3,17 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | zero pad');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = zeroPad([42]);
-  assert.ok(result);
+test('pads with zeros to the desired length', function(assert) {
+  let result = zeroPad([42, 3]);
+  assert.strictEqual(result, '042');
+});
+
+test('does not pad with zeros if input length equals desired length', function(assert) {
+	let result = zeroPad([42, 2]);
+	assert.strictEqual(result, '42');
+});
+
+test('does not pad with zeros if input length exceeds desired length', function(assert) {
+	let result = zeroPad([42, 1]);
+	assert.strictEqual(result, '42');
 });
