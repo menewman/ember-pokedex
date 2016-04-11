239c8bf62f217cfb9554e67ada3fd0858eb31bef
Michael Newman <daudadagr@gmail.com>
2016-04-10 20:04:51 -0500

    wrote unit tests for weight helper, fixing issue #6

--- tests/unit/helpers/weight-test.js
+++ tests/unit/helpers/weight-test.js
@@ -3,8 +3,17 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | weight');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = weight([42]);
-  assert.ok(result);
+test('converts input to kilograms and adds units', function(assert) {
+	let result = weight([42]);
+	assert.strictEqual(result, '4.2 kg');
+});
+
+test('rounds the weight to one decimal place', function(assert) {
+	let result = weight([4.231]);
+	assert.strictEqual(result, '0.4 kg');
+});
+
+test('always displays one decimal place, even if digit is 0', function(assert) {
+	let result = weight([40]);
+	assert.strictEqual(result, '4.0 kg');
 });
