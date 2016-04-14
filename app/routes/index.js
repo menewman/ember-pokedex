eb655b04ca9859c811f4b8397b8cf48d761d4d26
Michael Newman <daudadagr@gmail.com>
2016-04-14 18:52:09 -0500

    used bootstrap styles to clean up the look of the site

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,8 +4,14 @@ export default Ember.Route.extend({
   model() {
     return [
       { id: 1, name: 'bulbasaur' },
+      { id: 2, name: 'ivysaur' },
+      { id: 3, name: 'venusaur' },
       { id: 4, name: 'charmander' },
-      { id: 7, name: 'squirtle' }
+      { id: 5, name: 'charmeleon' },
+      { id: 6, name: 'charizard' },
+      { id: 7, name: 'squirtle' },
+      { id: 8, name: 'wartortle' },
+      { id: 9, name: 'blastoise' },
     ];
   }
 });
